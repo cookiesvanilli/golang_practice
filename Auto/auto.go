93af4f9097e6df94e9d04e57c7a18ff3a8adaeb6
cookies <in.d2010@yandex.ru>
2021-08-15 00:58:48 +0300

    auto

---
+++ Auto/auto.go
@@ -0,0 +1,43 @@
+//Существует набор структур: Колеса, Двигатель, Корпус. Колеса могут быть разные,
+//Двигатели тоже разные (например, 4 цилиндра, 8 цилиндров, дизель, бензиновый).
+//Корпуса могут быть седан, хэтчбек, универсал, можно добавить свои.
+//Собрать из этого набора несколько разных видов машин.
+package main
+
+import "fmt"
+
+type Automobile interface {
+	car()
+}
+type Wheels struct {
+    numberWheels int
+}
+type Engine struct {
+	cylinderNumb int
+	fuel string
+}
+type Corpus struct {
+	typeOfCar string
+}
+func (w *Wheels) car() {
+	fmt.Println("Wheels: ", w.numberWheels)
+}
+func (e *Engine) car()  {
+	fmt.Println("Cylinders: ", e.cylinderNumb)
+	fmt.Println("Fuel: ", e.fuel)
+}
+func (m *Corpus) car()  {
+	fmt.Println("Model: ", m.typeOfCar)
+}
+func myCar(list ...Automobile) {
+	for _, a := range list {
+		a.car()
+	}
+}
+func main() {
+	w := &Wheels{numberWheels: 4}
+	e := &Engine{cylinderNumb: 8, fuel: "diesel"}
+	m := &Corpus{typeOfCar: "sedan"}
+	car := []Automobile{w, e, m}
+	myCar(car...)
+}
