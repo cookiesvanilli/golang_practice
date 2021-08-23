b78c3d8583acf5fab83ba1f4e1ec33fd2cbd5927
cookies <in.d2010@yandex.ru>
2021-08-23 22:03:55 +0300

    airplane add

--- Auto/auto.go
+++ Auto/auto.go
@@ -8,6 +8,8 @@ import (
 	"fmt"
 )
 
+type AirplaneWings struct {}
+type BaggageHold struct {}
 type Tracks struct {}
 type Wheels struct {
     numberWheels int
@@ -17,17 +19,32 @@ type Engine struct {
 	fuel string
 }
 type Corpus struct {
-	typeOfCar string
+	typeOfTransport string
+}
+type Airplane struct {
+	a AirplaneWings
+	b BaggageHold
+	w Wheels
+	e Engine
+	c Corpus
 }
 type Car struct {
 	w Wheels
 	e Engine
 	c Corpus
+	b BaggageHold
+
 }
 type Tractor struct {
 	t Tracks
 	e Engine
 	c Corpus
+}
+func (a AirplaneWings) madeof() {
+	fmt.Println("Airplane wings")
+}
+func (b BaggageHold) madeof() {
+	fmt.Println("Baggage")
 }
 func (t Tracks) madeof() {
 	fmt.Println("Tracks")
@@ -40,12 +57,20 @@ func (e Engine) madeof()  {
 	fmt.Println("Fuel: ", e.fuel)
 }
 func (m Corpus) madeof()  {
-	fmt.Println("Model: ", m.typeOfCar)
+	fmt.Println("Type of Transport: ", m.typeOfTransport)
+}
+func (a Airplane) madeof() {
+	a.a.madeof()
+	a.b.madeof()
+	a.e.madeof()
+	a.w.madeof()
+	a.c.madeof()
 }
 func (c Car) madeof() {
 	c.w.madeof()
 	c.e.madeof()
 	c.c.madeof()
+	c.b.madeof()
 }
 func (t Tractor) madeof() {
 	t.t.madeof()
@@ -53,12 +78,25 @@ func (t Tractor) madeof() {
 	t.c.madeof()
 }
 func main() {
+	fmt.Println("Car =======")
 	w := Wheels{numberWheels: 4}
 	e := Engine{cylinderNumb: 8, fuel: "diesel"}
-	c := Corpus{typeOfCar: "sedan"}
-	car := Car{w, e, c}
+	c := Corpus{typeOfTransport: "sedan"}
+	b := BaggageHold{}
+	car := Car{w, e, c, b}
 	car.madeof()
+	fmt.Println("Tractor =======")
 	t := Tracks{}
-	tractor := Tractor{t, e, c}
+	tc := Corpus{typeOfTransport: "bulldozer"}
+	tractor := Tractor{t, e, tc}
 	tractor.madeof()
+	fmt.Println("Airplane =======")
+	a := AirplaneWings{}
+	h := BaggageHold{}
+	aw := Wheels{numberWheels: 2}
+	ae := Engine{cylinderNumb: 6, fuel: "petrol"}
+	ac := Corpus{typeOfTransport: "boat-shaped"}
+	plane := Airplane{a, h, aw, ae, ac}
+	plane.madeof()
+
 }
