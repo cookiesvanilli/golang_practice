f42e77b736b4466e8f09f344157a3f4acc294a68
cookies <in.d2010@yandex.ru>
2021-09-01 23:40:38 +0300

    reader

---
+++ FileWriteRead/fileReading.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+
+	fmt.Println("--- Scanner ---")
+	file, err := os.Open("hello.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	fmt.Println("--- Reader---")
+	reader := bufio.NewReader(file)
+	for {
+		line, err := reader.ReadString('\n')
+		fmt.Print(line)
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				log.Fatal(err)
+			}
+		}
+	}
+}
